9b4df444cdd34803d6af93300aad55280e93f0b5
Del1-SUDO <kurbonovs397@gmail.com>
2025-01-28 14:24:01 +0500

    done

---
+++ task1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Computer pc = new Computer ("Lenovo", "Beast", "Linux");
+        pc.Info();
+        pc.TurnOn();
+
+        System.Console.WriteLine();
+
+        Laptop laptop = new Laptop ("Acer", "Aspire 5", "Windows", 15);
+        laptop.Info();
+        laptop.TurnOn();
+        laptop.ScreenSizeInfo();
+
+        System.Console.WriteLine();
+
+        SmartPhone phone = new SmartPhone ("Iphone", "15 Pro", "IOS", 7);
+        phone.Info();
+        phone.TurnOn();
+        phone.PhoneInfo();
+    }
+}
+
+class Computer
+{
+    public string Brand;
+    public string Model;
+    public string OperatingSystem;
+
+    public Computer (string Brand, string Model, string OperatingSystem) {
+        this.Brand = Brand;
+        this.Model = Model;
+        this.OperatingSystem = OperatingSystem;
+    }
+
+    public void Info (){
+        System.Console.WriteLine($"Brand: {Brand}, Model: {Model}, OS: {OperatingSystem}");
+    }
+
+    public void TurnOn (){
+        System.Console.WriteLine($"The {Brand} {Model} is turned on. You are running {OperatingSystem}.");
+    }
+    
+}
+
+class Laptop : Computer
+{
+    public int ScreenSize;
+    public Laptop (string Brand, string Model, string OperatingSystem, int ScreenSize) : base (Brand, Model, OperatingSystem){
+        this.ScreenSize = ScreenSize;
+    }
+
+    public void ScreenSizeInfo(){
+        System.Console.WriteLine($"This {Brand} {Model} laptop has {ScreenSize} inch screen size.");
+    }
+
+}
+
+class SmartPhone : Computer
+{
+    public int BatteryLife;
+    public SmartPhone (string Brand, string Model, string OperatingSystem, int BatteryLife) : base (Brand, Model, OperatingSystem){
+        this.BatteryLife = BatteryLife;
+    }
+
+    public void PhoneInfo(){
+        System.Console.WriteLine($"This {Brand} {Model} phone has {BatteryLife} hours of battery life.");
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car("Toyota", "Camry", 180, 4);
+        car.DisplayInfo();
+
+        Console.WriteLine();
+
+        Airplane airplane = new Airplane("Boeing", "747", 900, 76);
+        airplane.DisplayInfo();
+
+        Console.WriteLine();
+
+        Train train = new Train("Siemens", "ICE 3", 300, 20);
+        train.DisplayInfo();
+
+        Console.WriteLine();
+
+        PassengerCar passengerCar = new PassengerCar("Mercedes", "E-Class", 220, 4, 5);
+        passengerCar.DisplayInfo();
+
+        Console.WriteLine();
+
+        CargoCar cargoCar = new CargoCar("Volvo", "FH", 120, 16, 20);
+        cargoCar.DisplayInfo();
+
+        Console.WriteLine();
+
+        PassengerPlane passengerPlane = new PassengerPlane("Airbus", "A380", 900, 75, 550);
+        passengerPlane.DisplayInfo();
+
+        Console.WriteLine();
+
+        CargoPlane cargoPlane = new CargoPlane("Boeing", "C-130", 800, 90, 30);
+        cargoPlane.DisplayInfo();
+    }
+}
+
+class Transport
+{
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public double Speed { get; set; }
+
+    public Transport(string brand, string model, double speed)
+    {
+        Brand = brand;
+        Model = model;
+        Speed = speed;
+    }
+
+    public virtual void DisplayInfo()
+    {
+        Console.WriteLine($"Transport: {Brand} {Model} traveling at {Speed} km/h");
+    }
+}
+
+class Car : Transport
+{
+    public int Wheels { get; set; }
+
+    public Car(string brand, string model, double speed, int Wheels) : base(brand, model, speed)
+    {
+        this.Wheels = Wheels;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"This car has {Wheels} wheels");
+    }
+}
+
+class Airplane : Transport
+{
+    public double Length { get; set; }
+
+    public Airplane(string brand, string model, double speed, double Length) : base(brand, model, speed)
+    {
+        this.Length = Length;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Length of the plane: {Length} meters");
+    }
+}
+
+class Train : Transport
+{
+    public int TonsOfGoods { get; set; }
+
+    public Train(string brand, string model, double speed, int TonsOfGoods) : base(brand, model, speed)
+    {
+        this.TonsOfGoods = TonsOfGoods;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Train can move up to: {TonsOfGoods} tons of goods.");
+    }
+}
+
+
+class PassengerCar : Car
+{
+    public int PassengerCapacity { get; set; }
+
+    public PassengerCar(string brand, string model, double speed, int Wheels, int passengerCapacity) : base(brand, model, speed, Wheels)
+    {
+        PassengerCapacity = passengerCapacity;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Passenger Capacity: {PassengerCapacity} people");
+    }
+}
+
+class CargoCar : Car
+{
+    public double CargoCapacity { get; set; }
+
+    public CargoCar(string brand, string model, double speed, int Wheels, double cargoCapacity) : base(brand, model, speed, Wheels)
+    {
+        CargoCapacity = cargoCapacity;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Cargo Capacity: {CargoCapacity} tons");
+    }
+}
+
+
+class PassengerPlane : Airplane
+{
+    public int PassengerCapacity { get; set; }
+
+    public PassengerPlane(string brand, string model, double speed, double Length, int PassengerCapacity) : base(brand, model, speed, Length)
+    {
+        this.PassengerCapacity = PassengerCapacity;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Passenger seats capacity: {PassengerCapacity} seats");
+    }
+}
+
+class CargoPlane : Airplane
+{
+    public double CargoCapacity { get; set; }
+
+    public CargoPlane(string brand, string model, double speed, double Length, double cargoCapacity) : base(brand, model, speed, Length)
+    {
+        CargoCapacity = cargoCapacity;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Cargo Capacity: {CargoCapacity} tons");
+    }
+}
